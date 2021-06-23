a04663050681da94003eeb7561e3568693dbfd45
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-06-23 18:21:44 -0300

    exercicio2

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = 5;
+
+let adicao = a + b;
+console.log(adicao);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
+
+let num1 = 3;
+let num2 = 5;
+
+if(num1 > num2) {
+    console.log("num1 maior que num2");
+} else {
+    console.log('num1 menor que num2');
+}

ff0e4115de914901b5f25157e3956b9c67578d21
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-06-23 18:33:29 -0300

    exercicio3

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -24,3 +24,16 @@ if(num1 > num2) {
 } else {
     console.log('num1 menor que num2');
 }
+
+let num1 = 3;
+let num2 = 5;
+let num3 = 6;
+
+if(num1 > num2 && num1 > num3) {
+    console.log('maior numero é o:' + num1 + '(num1)');
+} else if (num2 > num1 && num2 >num3) {
+    console.log('maior numero é o: (num2)');
+}
+else{
+    console.log('maior numero é o:' + num3 + '(num3)');
+}

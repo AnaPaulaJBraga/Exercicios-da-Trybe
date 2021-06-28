9632bab1af472ead232db3641dfe9e78954bc0b5
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-06-28 14:46:21 -0300

    ex ate 4

--- exercises/exercicio-aula.js
+++ exercises/exercicio-aula.js
@@ -7,8 +7,13 @@ let player = {
    medals : {
        golden : 2,
        silver : 3,
-   } 
+   },
+     bestInTheWorld : ['2006', '2007', '2008', '2009', '2010', '2018']
 }
 console.table (player)
 
 console.log("A jogadora" + " " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade." )
+
+console.log("A jogadora" + " " + player.name + " " + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes')
+
+console.log ("A jogadora possui " + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + " medalhas de prata.")

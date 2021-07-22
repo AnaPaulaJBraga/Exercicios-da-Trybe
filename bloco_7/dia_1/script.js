c34ab1393717cf9481f570a327e9ca2f10a3ea88
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-07-22 19:31:34 -0300

    exercicio ok

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -1,16 +1,19 @@
-// const testingScope = (escopo) => {
-//     if (escopo === true) {
-//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-//       console.log(ifScope);
-//     } else {
-//       const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//       console.log(elseScope);
-//     }
+// parte 1
+// exercicio 1
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
   
-// }
-//   testingScope(true);
+}
+  testingScope(true);
 
+//   exercicio 2
 
   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
@@ -26,3 +29,79 @@
   }
   const sortedArray = sortOddsAndEvens()
   console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`); 
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  const sortArrayBonus = array => {
+    const sortOddsAndEvens = array.sort((a, b) => a - b);
+    return sortOddsAndEvens;
+  }
+  
+  const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+  console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente !`);
+
+  oddsAndEvens.sort((a, b) => a - b);
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente !`);
+
+//   parte 2 
+// exercicio 1
+
+const factorial = number => {
+    let result = 1;
+
+    for (let index = 2; index <= number; index += 1) {
+        result *= index;
+    }
+
+    return result;
+}
+
+console.log(factorial(5));
+
+// exercicio 2
+
+const longestWord = text => {
+    let wordArray = text.split (" ");
+    let maxLength = 0;
+    let resultado = " ";
+
+    for(const word of wordArray) {
+        if (word.length > maxLength){
+            maxLength = word.length;
+            resultado = word;
+        }
+    }
+    return resultado
+}
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"))
+
+
+// exercicio 4
+const skills = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+function novoArray (param1){
+    const primeiraParte = paramInner => (
+        `Tryber ${paramInner} aqui!
+  
+        Tudo bem?`
+      );
+  
+      let result = `${primeiraParte(param1)}
+      Minhas cinco principais habilidades são:`
+
+      skills.forEach((skill, index) =>
+      result = `${result}
+  
+      - ${skill}`);
+  
+      result = `
+      ${result}
+  
+      #goTrybe
+      `;
+  
+      return result;
+  }
+  
+  console.log(novoArray("Lucas"));
+
+

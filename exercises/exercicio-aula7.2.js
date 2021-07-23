4cef8b385034a5e3d62215e4ce444057a8e93b4f
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-07-23 16:16:40 -0300

    ok

---
+++ exercises/exercicio-aula7.2.js
@@ -0,0 +1,135 @@
+const customer1 = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  console.log(customer1);
+  
+  customer1.lastName = 'Faria';
+  console.log(customer1);
+  
+  const customer2 = {
+    firstName: 'Maria',
+    age: 23,
+    job: 'Medic',
+  };
+  
+  console.log(customer2);
+  customer2['lastName'] = 'Silva';
+  console.log(customer2);
+
+
+  const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
+
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for(index in arrayOfSkills){
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
+
+const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsValuesWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsValuesWithValues = (student) => Object.values(student);
+  
+ 
+  
+  // Com Object.values
+  console.log(listSkillsValuesWithValues(student));
+
+
+
+  const países = {
+    França: 'Paris',
+    Brasil: 'Brasília',
+    Espanha: 'Madrid',
+    Portugal: 'Lisboa',
+  };
+  const pairKeyValue = Object.entries(países);
+  console.log(pairKeyValue);
+
+  for(index in pairKeyValue) {
+    console.log('--------');
+    console.log('País:', pairKeyValue[index][0]);
+    console.log('Capital:', pairKeyValue[index][1]);
+  };
+
+
+  const person = {
+    name: 'Alberto',
+    lastName: 'Gomes',
+    age: 20,
+  };
+  
+  const info = {
+    age: 23,
+    job: 'engenheiro',
+  };
+  
+  const family = {
+    children: ['Maria', 'João'],
+    wife: 'Ana',
+  };
+  
+  Object.assign(person, info, family)
+  console.log(person)
+
+  const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);

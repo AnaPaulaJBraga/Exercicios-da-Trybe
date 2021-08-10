882e100e881023a28d3e8bfafc46cc34f0a160bb
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-08-10 18:42:04 -0300

    exer 4

--- bloco_9/dia_1/script1.js
+++ bloco_9/dia_1/script1.js
@@ -1,30 +1,15 @@
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
-
-const mars = {
-  name: "Mars",
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: "kilometers",
-  },
-};
-
-const venus = {
-  name: "Venus",
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: "kilometers",
-  },
-};
-
-const jupiter = {
-  name: "Jupiter",
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: "kilometers",
-  },
-};
-
-console.log(planetDistanceFromSun(mars)); // A
-console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+const getPlanet = () => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    setTimeout(() => {
+        console.log("Returned planet: ", mars);
+    }, 4000);
+    
+  };
+  
+  getPlanet(); // imprime Marte depois de 4 segundos

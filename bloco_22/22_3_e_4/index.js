42e6da1c1fddd8dadbfb5f86cb74ff4fa90c1d0c
AnaPaulaBraga <anapaula.junq@hotmail.com>
2022-01-13 17:03:37 -0300

    exercicio 1 ao 4

--- bloco_22/22_3_e_4/index.js
+++ bloco_22/22_3_e_4/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express;
+const app = express();
 
 app.use(bodyParser.json());
+
+//ex.1
+app.get('/ping', (_req, res)=> res.json({"message": "pong"}));
+
+//ex.2
+app.post('/hello'), (req, res) => {
+    const {name} = req.body;
+    res.status(200).json({"message": `Hello, ${name}!`})
+};
+
+//ex. 3
+app.post('/greetings', (req, res)=>{
+    const {name, age} = req.body;
+    if(parseInt(age,10) <= 17){
+        return res.status(401).json({message: `Unauthorized`});
+    }
+
+    return res.status(200).json({message: `Hello, ${name}!`})
+});
+
+//ex.4
 
 
 app.use(function(err, req, res, next){
     res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
 
-app.listen(3000,()=> console.log('ouvindo na porta 3000!'));
+app.listen(3001,()=> console.log('ouvindo na porta 3001!'));

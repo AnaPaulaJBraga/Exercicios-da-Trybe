acf2c7311ba1e5956fc86d6cc0b793520317c98e
AnaPaulaBraga <anapaula.junq@hotmail.com>
2022-01-13 17:29:09 -0300

    5 e 6

--- bloco_22/22_3_e_4/index.js
+++ bloco_22/22_3_e_4/index.js
@@ -25,6 +25,36 @@ app.post('/greetings', (req, res)=>{
 });
 
 //ex.4
+app.put('/users/:name/:age', (req,res) =>{
+    const { name, age } = req.params;
+    res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade`})
+});
+
+//ex.5
+// const express = require('express');
+// const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+
+const simpsonsUtils = require('./fs-utils');
+
+app.get('/simpsons', rescue(async (req, res) => {
+  const simpsons = await simpsonsUtils.getSimpsons();
+
+  res.status(200).json(simpsons);
+}))
+
+//ex.6
+app.get('/simpsons/:id',rescue(async  (req, res) =>{
+    const simpsons = await simpsonsUtils.getSimpsons();
+    const simpson = simpsons.find(({ id }) => id === req.params.id);
+
+    if(!simpson) {
+        return res.status(404).json({ message: 'simpson not found' });
+    }
+    return res.status(202).json(simpson);
+})
+);
+
 
 
 app.use(function(err, req, res, next){
